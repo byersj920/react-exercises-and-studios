25c17bb822a6679ce1b65651ff17193ee3b2117d
Jonathan Byers <byersj920@gmail.com>
2023-11-15 16:05:05 -0600

    Exercise Submission

--- part2/exercises-2/src/components/Button.js
+++ part2/exercises-2/src/components/Button.js
@@ -2,11 +2,16 @@ import './styles.css';
 
 function Button() {
 
+function onLearnMore(){
+   window.alert("Fun fact: You're actually more likely to get bitten by a pig than a shark. Go swimming today!");
+}
    return ( 
-         <button>
+         <button onClick={onLearnMore}>
             Learn More
          </button>
    );
 }
+
+
 
 export default Button;
